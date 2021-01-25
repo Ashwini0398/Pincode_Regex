044c4c0c960b4d533741e9373ede1f48b6ee27eb
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-25 15:02:18 +0530

    ADDED: Regex pattern to validate 6 digit pincode

--- RegexPincodeMatch.java
+++ RegexPincodeMatch.java
@@ -5,7 +5,11 @@ public class RegexPincodeMatch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Pincode Match Using REjex");
+		System.out.println("Pincode Match Using Rejex");
+		Pattern pat = Pattern.compile("^[0-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}");
+		Matcher match = pat.matcher("400088");
+		boolean result=match.matches();
+		System.out.println(result);
 	
 	}
 }
