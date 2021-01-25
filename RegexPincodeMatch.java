18cb06c915af3911e37f31425f5fa6fb136ea14b
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-25 14:58:55 +0530

    ADDED: Messege

---
+++ RegexPincodeMatch.java
@@ -0,0 +1,11 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexPincodeMatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pincode Match Using REjex");
+	
+	}
+}
