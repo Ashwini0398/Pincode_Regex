1bf2b85baa89234f12fd00a46f879c002a485774
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-25 15:16:07 +0530

    MODIFIED: pattern to validate for alphabet or special character

--- RegexPincodeMatch.java
+++ RegexPincodeMatch.java
@@ -3,13 +3,35 @@ import java.util.regex.Pattern;
 
 public class RegexPincodeMatch {
 
+	public static boolean IsPinCodeValid(String pincode)
+	{
+		String regexPincode="^[0-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}" ;   //pattern 
+		Pattern pat = Pattern.compile(regexPincode);        //pattern entered to compile
+		
+		if(pincode== null)   //check wheather the pincode is null or not
+		{
+			return false;
+		}
+		Matcher match = pat.matcher(pincode);    //matches the pincode
+		
+		return match.matches(); //return if pincode match the regex
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Pincode Match Using Rejex");
-		Pattern pat = Pattern.compile("^[0-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}");
-		Matcher match = pat.matcher("400088");
-		boolean result=match.matches();
-		System.out.println(result);
+		
+		String n1="400088";     //6digit pincode  test1
+		System.out.println(n1+""+IsPinCodeValid(n1));
+		
+		String n2="A400088";   //special character or alphabet     
+		System.out.println(n2+""+IsPinCodeValid(n2));
+
 	
 	}
 }
+
+
+
+
+//output displays  400088 true
+//A400088-false
